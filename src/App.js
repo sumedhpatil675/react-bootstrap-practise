0e36c09911ba0cc40f29f116f0e7514d13d4ff15
Sumedh Patil <sumedhpatil675@gmail.com>
2022-10-16 15:06:37 +0530

    implemented counter funcationality

--- src/App.js
+++ src/App.js
@@ -4,16 +4,30 @@ import { Alert1 } from './components/Alert1';
 import { Container1 } from './components/Container1';
 import { Accordin1 } from './components/Accordin1';
 
+import {Navbar,Container} from 'react-bootstrap';
+import { Counter } from './components/Counter';
+
+
 
 
 function App() {
   return (
     <div className="App" >
          <>
-     <Button1/>
+     {/* <Button1/>
      <Alert1/>
      <Container1/>
-     <Accordin1/>
+     <Accordin1/> */}
+<Navbar bg="dark" expand="sm" variant='dark'>
+
+     <Container>
+
+        <Navbar.Brand href="/">React Bootrap</Navbar.Brand>
+
+     </Container>
+
+</Navbar>
+<Counter/>
 
     </>
     </div>
---
+++ src/components/Counter.js
@@ -0,0 +1,57 @@
+import React,{useState} from 'react'
+import {Container,Row,Col,Card,Button} from 'react-bootstrap';
+
+
+export const Counter = () => {
+
+        let [state,setState] = useState(
+         {
+            count:0,
+        });
+
+        let incr=()=>
+        {
+            setState({
+                count: state.count+1,
+            })
+        };
+        
+        let decr=()=>
+        {
+            setState({
+                count: state.count-1,
+            })
+        };
+
+
+
+    return (
+    <div>
+
+        <Container className="mt-3">
+
+            <Row>
+
+            <Col xs="4" >
+            
+        <Card className="shadow-lg">
+                    <Card.Body>
+                            <p className="display-5">{state.count}</p>
+                            <Button variant="success" onClick={incr}>+</Button>{' '}
+      <Button variant="danger" onClick={decr}>-</Button>{' '}
+
+                    </Card.Body>
+            
+            </Card>            
+            </Col>
+
+            </Row>
+
+
+
+        </Container>
+
+
+    </div>
+  )
+}
