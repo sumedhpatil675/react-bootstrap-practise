331ad0075dc70b3a0701e89de8823a3a1d29c29d
Sumedh Patil <sumedhpatil675@gmail.com>
2022-10-16 13:20:06 +0530

    added accordion

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Button1 from './components/Button1';
 import { Alert1 } from './components/Alert1';
 import { Container1 } from './components/Container1';
+import { Accordin1 } from './components/Accordin1';
 
 
 
@@ -12,6 +13,7 @@ function App() {
      <Button1/>
      <Alert1/>
      <Container1/>
+     <Accordin1/>
 
     </>
     </div>
